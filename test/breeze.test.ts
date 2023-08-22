eaff1783d966c3750671f85d77fc8c4455fe8776
Kathleen Hall <kathleenahall22@gmail.com>
2023-08-22 13:53:05 -0400

    fix test

--- test/breeze.test.ts
+++ test/breeze.test.ts
@@ -1,5 +1,5 @@
 test('Env subdomain and key variables are set', () => {
-  // expect(process.env.SUBDOMAIN).toEqual(expect.any(String));
-  // expect(process.env.KEY).toEqual(expect.any(String));
+  expect(process.env.SUB_DOMAIN).toEqual(expect.any(String));
+  expect(process.env.API_KEY).toEqual(expect.any(String));
   expect(true).toBe(true);
 });
